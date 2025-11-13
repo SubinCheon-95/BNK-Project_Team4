4210cc1906695e5baffb38597bef6edf9084508b
o-seoj <tjwjd010@naver.com>
2025-11-13 17:42:30 +0900

    관리자/회원 로그인 기능 추가, 회원 아이디 찾기 기능 추가

--- Busanbank/src/main/java/kr/co/busanbank/service/MemberService.java
+++ Busanbank/src/main/java/kr/co/busanbank/service/MemberService.java
@@ -1,74 +1,75 @@
-//package kr.co.busanbank.service;
-//
-//import kr.co.busanbank.project.dto.UsersDTO;
-//import kr.co.busanbank.project.mapper.MemberMapper;
-//import kr.co.busanbank.project.security.AESUtil;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//@Slf4j
-//@RequiredArgsConstructor
-//@Service
-//public class MemberService {
-//
-//    private final MemberMapper memberMapper;
-//
-//
-//    private final PasswordEncoder passwordEncoder;
-//    private final EmailService emailService;
-//
-//
-//    public void save(UsersDTO userDTO) throws Exception {
-//
-//        String encodedPass = passwordEncoder.encode(userDTO.getUserPw());
-//        String encodedAccountPass = passwordEncoder.encode(userDTO.getAccountPassword());
-//
-//        userDTO.setUserPw(encodedPass);
-//        userDTO.setAccountPassword(encodedAccountPass);
-//
-//        String key = AESUtil.generateKey();
-//
-//        String encryptedName = AESUtil.encrypt(userDTO.getUserName(), key);
-//        userDTO.setUserName(encryptedName);
-//
-//        String encryptedHp = AESUtil.encrypt(userDTO.getHp(), key);
-//        userDTO.setHp(encryptedHp);
-//
-//        String encryptedEmail = AESUtil.encrypt(userDTO.getEmail(), key);
-//        userDTO.setEmail(encryptedEmail);
-//
-//        String encryptedRrn = AESUtil.encrypt(userDTO.getRrn(), key);
-//        userDTO.setRrn(encryptedRrn);
-//
-//
-//        log.info("savedUserDTO = {}", userDTO);
-//
-//
-//        memberMapper.insertUser(userDTO);
-//    }
-//
-//
-//    public int countUser(String type, String value){
-//        int count = 0;
-//        if(type.equals("userId")){
-//            count = memberMapper.countByUserId(value);
-//        } else if(type.equals("email")){
-//            count = memberMapper.countByEmail(value);
-////            if(count == 0){
-////                emailService.sendCode(value);
-////            }
-//        } else if(type.equals("hp")){
-//            count = memberMapper.countByHp(value);
-//        }
-//        return count;
-//    }
-//
-//    public UsersDTO login(String userId, String userPw) {
-//        return memberMapper.login(userId, userPw);
-//    }
-//
-//
-//
-//}
+package kr.co.busanbank.service;
+
+import kr.co.busanbank.dto.UsersDTO;
+import kr.co.busanbank.mapper.MemberMapper;
+import kr.co.busanbank.security.AESUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberMapper memberMapper;
+
+
+    private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
+
+
+    public void save(UsersDTO userDTO) throws Exception {
+
+        String encodedPass = passwordEncoder.encode(userDTO.getUserPw());
+        String encodedAccountPass = passwordEncoder.encode(userDTO.getAccountPassword());
+
+        userDTO.setUserPw(encodedPass);
+        userDTO.setAccountPassword(encodedAccountPass);
+
+        userDTO.setUserName(AESUtil.encrypt(userDTO.getUserName()));
+        userDTO.setHp(AESUtil.encrypt(userDTO.getHp()));
+        userDTO.setEmail(AESUtil.encrypt(userDTO.getEmail()));
+        userDTO.setRrn(AESUtil.encrypt(userDTO.getRrn()));
+
+
+
+        log.info("savedUserDTO = {}", userDTO);
+
+
+        memberMapper.insertUser(userDTO);
+    }
+
+
+    public int countUser(String type, String value){
+        int count = 0;
+        if(type.equals("userId")){
+            count = memberMapper.countByUserId(value);
+        } else if(type.equals("email")){
+            count = memberMapper.countByEmail(value);
+//            if(count == 0){
+//                emailService.sendCode(value);
+//            }
+        } else if(type.equals("hp")){
+            count = memberMapper.countByHp(value);
+        }
+        return count;
+    }
+
+    public UsersDTO getUserIdInfoEmail(String userName, String email) throws Exception {
+        String encryptedName = AESUtil.encrypt(userName);
+        String encryptedEmail = AESUtil.encrypt(email);
+        log.info("encryptedName: {}, encryptedEmail: {}", encryptedName, encryptedEmail);
+        return memberMapper.findUserIdInfoEmail(encryptedName, encryptedEmail);
+    }
+
+
+    public UsersDTO getUserIdInfoHp(String userName, String hp) throws Exception {
+        String encryptedName = AESUtil.encrypt(userName);
+        String encryptedHp = AESUtil.encrypt(hp);
+        return memberMapper.findUserIdInfoHp(encryptedName, encryptedHp);
+    }
+
+
+}
