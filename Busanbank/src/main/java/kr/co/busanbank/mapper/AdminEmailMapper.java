9e9eb659df661cb92fd6fcbc13f99c669126c959
whddls0323 <whddls0323@naver.com>
2025-11-20 17:28:17 +0900

    관리자 이메일 상담 제작중

---
+++ Busanbank/src/main/java/kr/co/busanbank/mapper/AdminEmailMapper.java
@@ -0,0 +1,17 @@
+package kr.co.busanbank.mapper;
+
+import kr.co.busanbank.dto.EmailCounselDTO;
+import kr.co.busanbank.dto.PageRequestDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface AdminEmailMapper {
+
+    public EmailCounselDTO findById(int ecounselId);
+
+    public List<EmailCounselDTO> findAll(@Param("pageRequestDTO") PageRequestDTO pageRequestDTO);
+    public int selectCount(@Param("pageRequestDTO")  PageRequestDTO pageRequestDTO);
+}
